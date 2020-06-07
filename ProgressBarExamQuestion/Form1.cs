628e5e8b897c97cbc70229aa37d32305adbe87af
Rezo Megrelidze <rezomegrelidze@gmail.com>
2020-06-07 20:17:37 +0300

    Add project files.

---
+++ ProgressBarExamQuestion/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarExamQuestion
+{
+    public partial class Form1 : Form
+    {
+        private int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += (o, args) =>
+            {
+                if(counter == 100) timer.Stop();
+                label1.Text = $"{counter}%";
+                progressBar1.Value = counter;
+                counter++;
+
+            };
+            timer.Start();
+        }
+    }
+}
